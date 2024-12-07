04dd1ae0c857e0c8d84ad650c74e0f4a5637982d
Evandro Rhari <93932260+E-rhari@users.noreply.github.com>
2024-12-07 12:06:19 -0300

    Focus v0.1

--- Assets/Scripts/Player/PlayerCharacter.cs
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -7,19 +7,31 @@ public abstract class PlayerCharacter : MonoBehaviour
      [SerializeField] protected float baseSpeed = 5f;
     private float currentSpeed;
     // protected SpriteRenderer spriteRenderer;
+    protected Hitbox hitbox;
 
 
     protected abstract void Shoot();
 
 
     protected void Movement(){
+        Focus();
         Vector3 displacement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))*currentSpeed;
         transform.Translate(displacement*Time.deltaTime); 
     }
 
+    protected void Focus(){
+        if(Input.GetKey(KeyCode.LeftShift)){
+            currentSpeed = baseSpeed/2;
+            hitbox.Show();
+        }
+        else{
+            currentSpeed = baseSpeed;
+            hitbox.Hide();
+        }
     }
 
     protected void GetPendentComponents(){
         // spriteRenderer = GetComponent<SpriteRenderer>();
+        hitbox = GetComponentInChildren<Hitbox>();
     }
 }
