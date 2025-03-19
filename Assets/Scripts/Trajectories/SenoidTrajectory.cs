bee02a47538d62984be97aa10fe748267790c6c3
Evandro Rhari <93932260+E-rhari@users.noreply.github.com>
2025-03-19 15:45:06 -0300

    Upgraded Senoid Trajectory

--- Assets/Scripts/Trajectories/SenoidTrajectory.cs
+++ Assets/Scripts/Trajectories/SenoidTrajectory.cs
@@ -9,6 +9,12 @@ public class SenoidTrajectory : TrajectoryBehaviour
     private Vector3 displacement = new Vector3(0,0);
     private Vector3 relativeCenter = new Vector3(0,0);
 
+    [SerializeField] private float amplitude = 1f;
+    [SerializeField] private float lenght = 1f;
+    [SerializeField] private float startDisplacement = 0f;
+    [SerializeField] private float positionDisplacement = 0f;
+    private float sinPosition;
+
     private Vector3 center;
 
 
@@ -32,10 +38,11 @@ public class SenoidTrajectory : TrajectoryBehaviour
         time += Time.deltaTime;
         center = transform.position - relativeCenter;
 
+        sinPosition = amplitude * Mathf.Sin(lenght*time*speed + startDisplacement) + positionDisplacement;
         if(direction == Direction.Horizontal || direction == Direction.All)
-            displacement += new Vector3(Mathf.Sin(time*speed)-relativeCenter.x, 0);
+            displacement += new Vector3(sinPosition-relativeCenter.x, 0);
         else if(direction == Direction.Vertical || direction == Direction.All)
-            displacement += new Vector3(0, Mathf.Sin(time*speed)-relativeCenter.y);
+            displacement += new Vector3(0, sinPosition-relativeCenter.y);
 
         transform.position += displacement;
         relativeCenter = transform.position - center;
